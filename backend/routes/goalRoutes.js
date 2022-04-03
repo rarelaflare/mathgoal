377b35ce83f63f5ec2a03610df77e8c263289b32
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2022-04-03 13:59:56 -0700

    initial route folder and file commit

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Get Goals..'
+    })
+})
+
+router.post('/', (req,res) => {
+    res.status(200).json({
+        message: "Set Goals.."
+    })
+})
+
+router.put('/:id', (req,res) => {
+    res.status(200).json({
+        message: `Update goals ${req.params.id}`
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({
+        message: `Delete goals ${req.params.id}`
+    })
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const express = require('express')
 // require dotenv 
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
-const fs = require('fs')
+//const fs = require('fs')
 
 const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
 
-console.log("The Marathon!..." + fs );
+console.log("The Marathon!..." );
