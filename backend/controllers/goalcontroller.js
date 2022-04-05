ff661bd6471759ed04a01e40b2bccb2f2dd3ce7b
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2022-04-05 01:02:15 -0700

    unsaved section

--- backend/controllers/goalcontroller.js
+++ backend/controllers/goalcontroller.js
@@ -1,18 +1,19 @@
+const asyncHandler = require('express-async-handler')
 // @desc Get goals initally
 // @route GET /api/goals
 // @access Private
 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: 'Get goals.. The Marathon Continues'
     })
-}
+})
 
 // @desc Set goals 
 // @route POST /api/goals
 // @access Private
 
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please add text field')
@@ -21,27 +22,27 @@ const setGoals = (req, res) => {
     res.status(200).json({
         message: 'Set goals.. This is a Sprint not a Lap!...'
     })
-}
+})
 
 // @desc Update goals 
 // @route PUT /api/goals
 // @access Private
 
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Update goals ${req.params.id}`
     })
-}
+})
 
 // @desc Delete goals 
 // @route DELETE /api/goals
 // @access Private
 
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Delete goals ${req.params.id}`
     })
-}
+})
 
 module.exports = {
     getGoals,
