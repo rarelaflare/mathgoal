72a5d67404791b098d94ba56deee36393daba109
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2022-04-05 00:59:37 -0700

    routes and controllers setup

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,19 @@ const express = require('express')
 // require dotenv 
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
 //const fs = require('fs')
 
 const app = express()
 
+// Middleware
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port: ${port}`))
 
